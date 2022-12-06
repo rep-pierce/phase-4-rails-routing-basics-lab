fe2c9fe3162e940512a3e28b8baa08338bf49471
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-12-06 14:37:17 -0700

    commit tree 5385cefad10e7da6231e97e14c84fc39f33b9e3e

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,10 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+    def grades
+        students = Student.all.sort_by { |student| student.grade}.reverse
+        render json: students
+    end
+end
